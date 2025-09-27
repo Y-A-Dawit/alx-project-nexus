d8f785f7eb9e230df31926081a5a884f17685c1d
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-09-28 01:36:23 +0300

    run migration, and switched to posgresql from sqlite3

--- online-poll-backend/polls_api/settings.py
+++ online-poll-backend/polls_api/settings.py
@@ -9,6 +9,10 @@ from pathlib import Path
 # Build paths inside the project
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# load .env
+from dotenv import load_dotenv
+load_dotenv(BASE_DIR / ".env")
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")  # fallback for dev
 
