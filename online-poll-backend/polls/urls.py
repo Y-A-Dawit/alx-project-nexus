e608de226eb0ef6af82c46dae094f1473bcae676
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-09-28 00:13:15 +0300

    added urls.py file inside the polls app

---
+++ online-poll-backend/polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PollViewSet, VoteAPIView, ResultsAPIView
+
+router = DefaultRouter()
+router.register(r"polls", PollViewSet, basename="poll")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("polls/<int:poll_id>/vote/", VoteAPIView.as_view(), name="poll-vote"),
+    path("polls/<int:poll_id>/results/", ResultsAPIView.as_view(), name="poll-results"),
+]
