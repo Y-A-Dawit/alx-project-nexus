70a35b6aec5aea44d356251019c2fdd803d8abe0
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-09-27 23:58:27 +0300

    modified models.py file, and edited admin.py file for creating the 3 data models

--- online-poll-backend/polls/admin.py
+++ online-poll-backend/polls/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Poll, Choice, Vote
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ("id", "question", "created_at", "expiry_date")
+    inlines = [ChoiceInline]
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "poll", "choice_text")
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ("id", "poll", "choice", "voter_id", "voted_at")
+    search_fields = ("voter_id",)
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Vote, VoteAdmin)
