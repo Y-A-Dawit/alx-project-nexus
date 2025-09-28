4e8bb92af3bdbfc626ae65ec5f5957e2512b170c
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-09-28 04:13:52 +0300

    modifying/editing files for deployment

--- online-poll-backend/polls_api/settings.py
+++ online-poll-backend/polls_api/settings.py
@@ -14,10 +14,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")  # fallback for dev
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")  # fallback for dev
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "1") == "1"
+DEBUG = os.getenv("DEBUG", "0") == "1"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
@@ -76,7 +76,7 @@ if os.getenv("DB_NAME"):
             "NAME": os.getenv("DB_NAME"),
             "USER": os.getenv("DB_USER"),
             "PASSWORD": os.getenv("DB_PASSWORD"),
-            "HOST": os.getenv("DB_HOST", "localhost"),
+            "HOST": os.getenv("DB_HOST"),
             "PORT": os.getenv("DB_PORT", "5432"),
         }
     }
