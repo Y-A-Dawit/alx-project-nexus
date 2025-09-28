7d1ce942e55fbfb317324242ceee4acb920bd752
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-09-28 06:14:33 +0300

    update

--- polls_api/urls.py
+++ polls_api/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
+from django.shortcuts import redirect
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -31,7 +32,11 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+def redirect_to_docs(request):
+    return redirect('/api/docs/')
+
 urlpatterns = [
+    path('', redirect_to_docs)
     path("admin/", admin.site.urls),
     path("api/", include("polls.urls")),
     re_path(r"^api/docs(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
