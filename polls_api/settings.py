5bf1526631d15eead54a368dd6af17ec3c4522b5
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-09-28 18:27:08 +0300

    update

--- polls_api/settings.py
+++ polls_api/settings.py
@@ -113,9 +113,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-MEDIA_URL = "/media"
-MEDIA_ROOT = BASE_DIR / "media"
-
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
