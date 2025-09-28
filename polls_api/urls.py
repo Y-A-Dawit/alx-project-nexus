3e0e79d641511e83701349a28129622371e38a6d
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-09-28 19:52:00 +0300

    update

--- polls_api/urls.py
+++ polls_api/urls.py
@@ -20,7 +20,8 @@ from django.conf import settings
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from .views import home
+from polls.views import home as polls_home
+from polls_api.views import home as api_home
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -33,7 +34,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('', home), # root url
+    path('', api_home, name="api-home"), # root url
     path("admin/", admin.site.urls),
     path("api/", include("polls.urls")),
     re_path(r"^api/docs(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
